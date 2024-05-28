c3121fca4cb9c8d130858c413a48ba0599debc75
João Victor <joaofdias.dev@gmail.com>
2024-05-28 19:22:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Error404 from './components/Error404';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Error404 />
     </div>
   );
 }
---
+++ src/components/Error404.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import myImage from '../assets/images/image.png'; // Ajuste o caminho da imagem conforme necessário
+
+function Error404() {
+  return (
+    <div className="absolute inset-0 flex flex-col items-center justify-center p-4 md:p-8 lg:p-12 -mt-12">
+      <img src={myImage} alt="Error 404" className="mb-8 w-2/4 max-w-lg" /> {/* Aumentando a largura da imagem */}
+      <div className="p-4 rounded-lg shadow-lg text-center" style={{ border: '4px solid #de186b' }}>
+        <h1 className="text-8xl font-bold text-white mb-4">Error 404</h1>
+        <p className="text-lg text-white">Page Not Found</p>
+        <p className="mt-4 text-base text-white">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Quisque sed mi ultrices, mollis arcu nec, fermentum justo. 
+          Donec dapibus, lorem ac hendrerit sodales.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Error404;
