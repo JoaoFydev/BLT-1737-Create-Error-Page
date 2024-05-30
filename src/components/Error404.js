05028f03ebc03304464aa6643c3cf52c69305f45
João Victor <joaofdias.dev@gmail.com>
2024-05-30 14:16:59 -0300

    Alterado bordas e centralizado text

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -5,8 +5,8 @@ function Error404() {
   return (
     <div className="absolute inset-0 flex flex-col items-center justify-center p-4 md:p-8 lg:p-12 -mt-12">
       <img src={myImage} alt="Error 404" className="mb-8 w-2/4 max-w-lg" /> 
-      <div className="p-4 rounded-lg shadow-lg text-center" style={{ border: '4px solid #de186b' }}>
-        <h1 className="text-8xl font-bold text-white mb-4">Error 404</h1>
+      <div className="w-2/4 max-w-lg text-center">
+        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-white mb-4">Error 404</h1>
         <p className="text-lg text-white">Page Not Found</p>
         <p className="mt-4 text-base text-white">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
